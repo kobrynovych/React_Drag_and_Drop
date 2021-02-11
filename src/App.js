3df3e7352de81dc0d66f303d859963e326892d9d
IgorKob <kobrynovych@gmail.com>
2021-02-11 19:12:47 +0200

    add 2 group

--- src/App.js
+++ src/App.js
@@ -40,32 +40,126 @@ const finalProducts = [
   }
 ]
 
+const finalProducts2 = [
+  {
+    id: '6',
+    name: 'картофель',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p1.jpg'
+  },
+  {
+    id: '7',
+    name: 'помидоры',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p1.jpg'
+  },
+  {
+    id: '8',
+    name: 'клубника',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p2.jpg'
+  },
+  {
+    id: '9',
+    name: 'шоколад',
+    groupId: 8,
+    groupName: 'кондитерские изделия',
+    thumb: '/images/p3.jpg'
+  },
+  {
+    id: '10',
+    name: 'томаты',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p4.jpg'
+  }
+]
+
 const App = () => {
   const [products, setProducts] = useState(finalProducts);
+  const [products2, setProducts2] = useState(finalProducts2);
 
   function handleOnDragEnd(result) {
+    debugger
+
     if (!result.destination) return;
 
-    const items = Array.from(products);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+    if (result.destination.droppableId === "products") {
+      debugger
 
-    setProducts(items);
+      if (result.source.droppableId === "products") {
+        debugger
+        const items = Array.from(products);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        // items.splice(0, 0, reorderedItem);
+        setProducts(items);
+      }
+
+      if (result.source.droppableId === "products2") {
+        debugger
+        const items2 = Array.from(products2);
+        const [reorderedItem2] = items2.splice(result.source.index, 1);
+        const items = Array.from(products);
+        items.splice(result.destination.index, 0, reorderedItem2);
+        setProducts(items);
+        setProducts2(items2);
+      }
+    }
+
+    if (result.destination.droppableId === "products2") {
+      debugger
+
+      if (result.source.droppableId === "products") {
+        debugger
+        const items = Array.from(products);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+
+        const items2 = Array.from(products2);
+        items2.splice(result.destination.index, 0, reorderedItem);
+        setProducts(items);
+        setProducts2(items2);
+      }
+
+      if (result.source.droppableId === "products2") {
+        debugger
+        const items2 = Array.from(products2);
+        const [reorderedItem2] = items2.splice(result.source.index, 1);
+        items2.splice(result.destination.index, 0, reorderedItem2);
+        setProducts2(items2);
+      }
+    }
   }
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Final products</h1>
         <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="products">
-            {(provided) => (
-              <ul className="products" {...provided.droppableProps} ref={provided.innerRef}>
+          <div>1</div>
+          <Droppable droppableId="products" type='asd'>
+            {(provided, snapshot) => (
+              <ul className="products" 
+                {...provided.droppableProps} 
+                ref={provided.innerRef}
+                // style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+              >
                 {products.map(({id, name, thumb}, index) => {
                   return (
                     <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
+                      {(provided, snapshot) => {
+                        // const style = {
+                        //   backgroundColor: snapshot.isDragging ? 'blue' : 'white',
+                        //   fontSize: 18,
+                        //   ...provided.draggableProps.style,
+                        // };
+                        return (
                         <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        {/* <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={style}> */}
                           <div className="products-thumb">
                             <img src={thumb} alt={`${name} Thumb`} />
                           </div>
@@ -73,7 +167,44 @@ const App = () => {
                             { name }
                           </p>
                         </li>
-                      )}
+                        )}}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+
+          <div>2</div>
+          
+          <Droppable droppableId="products2" type='asd'>
+            {(provided, snapshot) => (
+              <ul className="products" 
+                {...provided.droppableProps} 
+                ref={provided.innerRef}
+                // style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+              >
+                {products2.map(({id, name, thumb}, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided, snapshot) => {
+                        // const style = {
+                        //   backgroundColor: snapshot.isDragging ? 'blue' : 'white',
+                        //   fontSize: 18,
+                        //   ...provided.draggableProps.style,
+                        // };
+                        return (
+                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        {/* <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={style}> */}
+                          <div className="products-thumb">
+                            <img src={thumb} alt={`${name} Thumb`} />
+                          </div>
+                          <p>
+                            { name }
+                          </p>
+                        </li>
+                        )}}
                     </Draggable>
                   );
                 })}
