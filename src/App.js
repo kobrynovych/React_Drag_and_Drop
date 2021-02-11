5a9a88a5ed8c51e0d7b20c8ccda8581ddc1d6d96
IgorKob <kobrynovych@gmail.com>
2021-02-11 17:09:16 +0200

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 
-function App() {
+const finalProducts = [
+  {
+    id: '1',
+    name: 'картофель',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p1.jpg'
+  },
+  {
+    id: '2',
+    name: 'помидоры',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p1.jpg'
+  },
+  {
+    id: '3',
+    name: 'клубника',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p2.jpg'
+  },
+  {
+    id: '4',
+    name: 'шоколад',
+    groupId: 8,
+    groupName: 'кондитерские изделия',
+    thumb: '/images/p3.jpg'
+  },
+  {
+    id: '5',
+    name: 'томаты',
+    groupId: 21,
+    groupName: 'овощи и фрукты',
+    thumb: '/images/p4.jpg'
+  }
+]
+
+const App = () => {
+  const [products, setProducts] = useState(finalProducts);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(products);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setProducts(items);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Final products</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="products">
+            {(provided) => (
+              <ul className="products" {...provided.droppableProps} ref={provided.innerRef}>
+                {products.map(({id, name, thumb}, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          <div className="products-thumb">
+                            <img src={thumb} alt={`${name} Thumb`} />
+                          </div>
+                          <p>
+                            { name }
+                          </p>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
       </header>
     </div>
   );
 }
-
 export default App;
